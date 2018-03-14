92be7e3007c76fd2890ac9ad0418a6d924152546
Alberto <jalberto.robles@outlook.com>
2018-03-13 19:17:00 -0700

    Almost there

--- js/script.js
+++ js/script.js
@@ -1,19 +1,39 @@
 $(function(){
-var a = 1;
-    var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+
+  $(".load-gif").hide();
+    
+
+$(".select-box").change(function(){
+  
+  var selection = $(".select-box").val();
+  console.log(selection);
+
+  var url = "https://api.nytimes.com/svc/topstories/v2/"+selection+".json";
 url += '?' + $.param({'api-key': "58e7447484524db69db97ecdede76026"});
 
+  $(".load-gif").show();
+  
+  $.ajax({
+    url: url,
+    method: 'GET',
+    dataType: "json"
+  }).done(function(result) {
+    console.log(result);
+    $(".load-gif").hide();
+    $(".news-feed").empty();
+    for (var i=0; i<12; ++i){
+      $(".news-feed").append("<a href='"+result.results[i].url+"' class='article-link'><div class='article' style='background-image: url("+result.results[i].multimedia[4].url+")'><h2 class='news-text'>"+result.results[i].abstract+"</h2></div></a>");
+    }
 
-
-$.ajax({
-  url: url,
-  method: 'GET',
-  dataType: "json"
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
-
+  
+    // $.each(result.results, function(key, value){
+    //   console.log(value);
+    //   $(".news-feed").append("<a href='"+value.url+"' class='article-link'><div class='article' style='background-image: url("+value.multimedia[4].url+")'><h2 class='news-text'>"+value.abstract+"</h2></div></a>");
+    // })
+  }).fail(function(err) {
+    throw err;
+  });
+  
+})
 
 });
